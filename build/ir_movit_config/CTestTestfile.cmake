baed8eb3f377d0e840104bbfb099cf06b2ac3f22
Giacomo <giacomo.vianello.2@studenti.unipd.it>
2025-11-15 21:16:22 +0100

    Remove build/install/log artifacts from repo

--- build/ir_movit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/giacomo/IR_LABS/Assignments/ws_07_assignments/src/ir_2526/ir_movit_config
-# Build directory: /home/giacomo/IR_LABS/Assignments/ws_07_assignments/build/ir_movit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
