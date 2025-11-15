4a223804f18e804a792d754fc11f423a64c8612e
Giacomo <giacomo.vianello.2@studenti.unipd.it>
2025-11-15 21:13:51 +0100

    Minor Fixes

---
+++ build/assignment_1_07/CMakeFiles/nav2_orchestrator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nav2_orchestrator.dir/src/nav2_orchestrator.cpp.o"
+  "CMakeFiles/nav2_orchestrator.dir/src/nav2_orchestrator.cpp.o.d"
+  "nav2_orchestrator"
+  "nav2_orchestrator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nav2_orchestrator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
