76c6c33bc563e3f8f91b686afdd0ce5f5cc707be
Giacomo <giacomo.vianello.2@studenti.unipd.it>
2025-11-15 22:19:40 +0100

    Minor fixes

---
+++ build/assignment_1_07/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/giacomo/IR_LABS/Assignments/ws_07_assignments/src/assignment_1_07
+# Build directory: /home/giacomo/IR_LABS/Assignments/ws_07_assignments/build/assignment_1_07
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
